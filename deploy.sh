9482abb9d7fdd4b2749e677dd43071bd58239d77
sunhl-y <1192612803@qq.com>
2022-05-25 16:04:05 +0800

    feat: 初始化菜单

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,25 @@
-# 构建
-npm run build
-# 导航到构建输出目录
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
 cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# 推到你仓库的 master 分支
-git push -f git@github.com:sunhl-y/vuepressBlog.git master
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:sunhl-y/vuepressBlog.git master:gh-pages
+
+cd -
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,22 +1,98 @@
 module.exports = {
-    title: 'blog',
-    description: '个人网站',
-    base: '/vuepressBlog/', 
-    markdown: {
-      lineNumbers: false // 代码块显示行号
-    },
-    themeConfig: {
-      nav:[ 
-      { text: '首页', link:'/' },
+  title: '今天你学习了吗',
+  description: 'personal website',
+  base: '/vuepressBlog/',
+  head: [
+    ['link', { rel: 'icon', href: '/logo.jpg' }]
+  ],
+  markdown: {
+    // 代码块是否显示行号
+    lineNumbers: false 
+  },
+  themeConfig: {
+    logo: '/logo.jpg',
+    nav: [
+      { text: '首页', link: '/index.md' },
+      { text: '计算机基础', link: '/foundation/' },
+      { text: 'Java', link: '/java/' },
+      { text: 'Go', link: '/go/' },
+      // 下拉格式
       {
-        text:'blog',
+        text: '个人',
         items: [
-          {text: 'Java基础', link: '/accumulate/' },
-          {text: '图解算法', link: '/algorithm/'},
-          {text: '手摸手造一个RPC', link: 'http://dubbo.io/'}    
-                      ]
-                  }
-              ]
-      }
-  }
-  
+          { text: '书籍', link: '/xr/booklist/' },
+          { text: '电影', link: '/xr/filmlist/' }
+        ]
+      },
+      { text: '关于我', link: '/xr/introduction.md' },
+      {
+        text: '其他',
+        items: [
+          { text: '迭代记录', link: '/others/update.md' }
+        ]
+      },
+    ],
+    // 为以下路由添加左侧边栏
+    sidebar: {
+      '/java/': [{
+        title: 'Java学习笔记',
+        collapsable: false, // 是否可以收起来
+        children: [
+          {
+            title: 'Java基础', path: '/java/并发编程.md'
+          },
+          {
+            title: '并发编程', path: '/java/并发编程.md'
+          },
+          {
+            title: 'MySQL', path: '/java/并发编程.md'
+          },
+          {
+            title: 'JVM', path: '/java/并发编程.md'
+          },
+          {
+            title: '多线程', path: '/java/多线程.md'
+          }
+        ]
+      }],
+      '/foundation/': [{
+        title: '计算机基础',
+        collapsable: false, // 是否可以收起来
+        children: [
+          {
+            title: '计算机网络', path: '/java/并发编程.md'
+          },
+          {
+            title: '操作系统', path: '/java/并发编程.md'
+          },
+          {
+            title: '数据结构与算法', path: '/java/并发编程.md'
+          }
+        ]
+      }],
+      '/xr/booklist/': [{
+        title: '读书清单',
+        collapsable: false, // 是否可以收起来
+        children: [
+          {
+            title: '2022', path: '/java/并发编程.md'
+          },
+          {
+            title: '2021', path: '/java/并发编程.md'
+          },
+          {
+            title: '2020', path: '/java/并发编程.md'
+          }
+        ]
+      }]
+    },
+    //左侧导航显示的层级
+    sidebarDepth: 2,
+    // 回到顶层
+    plugins: ['@vuepress/back-to-top'],
+    // 显示最近更新时间
+    lastUpdated: 'Last Updated',
+    // 页面滚动
+    smoothScroll: true
+  },
+}
